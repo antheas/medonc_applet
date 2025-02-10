f5e16ddf32d29866a74fbe93aeaa900147f06a05
antheas <antheas@users.noreply.github.com>
2025-02-10 00:02:54 +0000

    remake loading functions to support other data

--- src/guessgame/base.py
+++ src/guessgame/base.py
@@ -1,14 +1,13 @@
 from flask import Flask, render_template, request
 import random
-
-from .ds import generate_patient, load_data
+import logging
+from .ds import generate_patient, load_data, Experiment
 
 app = Flask(__name__)
 
-ds_path = None
-countdown = 15
-ds_names = {"latest": "Latest"}
-datasets = {}
+logger = logging.getLogger(__name__)
+
+experiment = {}
 
 
 def get_relative_fn(fn: str):
@@ -21,69 +20,64 @@ def get_relative_fn(fn: str):
     return os.path.join(dirname, fn)
 
 
-@app.route("/")
-def index():
-    return render_template("index.html", datasets=ds_names)
+# @app.route("/")
+# def index():
+#     return render_template("index.html", datasets=datasets)
 
 
-@app.route("/game")
-def game():
-    syn = random.randint(0, 1) == 1
-    patient = (
-        f"No patient data (running in fake mode or generation failed)\nSynth: {syn}"
-    )
+# @app.route("/game")
+# def game():
+#     syn = random.randint(0, 1) == 1
+#     patient = (
+#         f"No patient data (running in fake mode or generation failed)\nSynth: {syn}"
+#     )
 
-    dataset = None
-    dataset_name = request.args.get("dataset")
-    if dataset_name in datasets:
-        dataset = datasets[dataset_name]
-    elif ds_path and ds_path != "skip":
-        dataset = load_data(ds_path, dataset_name)
-        datasets[dataset_name] = dataset
+#     dataset = None
+#     dataset_name = request.args.get("dataset")
+#     if dataset_name in datasets:
+#         dataset = datasets[dataset_name]
+#     elif ds_path and ds_path != "skip":
+#         dataset = load_data(ds_path, dataset_name)
+#         datasets[dataset_name] = dataset
 
-    if dataset:
-        for _ in range(10):
-            try:
-                patient = generate_patient(dataset, syn)
-                break
-            except Exception:
-                pass
+#     if dataset:
+#         for _ in range(10):
+#             try:
+#                 patient = generate_patient(dataset, syn)
+#                 break
+#             except Exception:
+#                 pass
 
-    return render_template(
-        "game.html",
-        patient=patient,
-        fake="true" if syn else "false",
-        countdown=countdown,
-    )
+#     return render_template(
+#         "game.html",
+#         patient=patient,
+#         fake="true" if syn else "false",
+#         countdown=countdown,
+#     )
 
 
-@app.route("/results")
-def results():
-    return render_template("results.html", datasets=ds_names)
+# @app.route("/results")
+# def results():
+#     return render_template("results.html", datasets=datasets)
 
 
 def main():
     import sys
 
-    if len(sys.argv) < 3:
-        print(
-            "Missing pasteur dataset path or datasets. Enter guessgame <path> <datasets> [args]"
-        )
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
+
+    if len(sys.argv) < 2:
+        print("Missing dataset path. Enter guessgame <path> [args]")
         sys.exit(1)
 
-    global ds_path, ds_names, countdown
     ds_path = sys.argv[1]
-    ds_names = {}
-    for ds in sys.argv[2].split(","):
-        k, v = ds.split(":", 1)
-        ds_names[k] = v
 
-    if ds_path == "skip":
-        print("Skipping dataset loading")
-
-    for arg in sys.argv:
-        if arg.startswith("countdown="):
-            countdown = int(arg.split("=", 1)[1])
+    global experiment
+    try:
+        experiment = load_data(ds_path)
+    except AssertionError as e:
+        logger.error(e)
+        return
 
     if "--browser" in sys.argv:
         import webbrowser
@@ -93,7 +87,7 @@ def main():
         webbrowser.open("http://localhost:6565/")
     else:
         app.debug = True
-    
+
     app.run(
         host="127.0.0.1",
         port=6565,
--- src/guessgame/ds.py
+++ src/guessgame/ds.py
@@ -1,11 +1,32 @@
 import re
 import math
 import os
-from typing import NamedTuple
+import json
+from typing import NamedTuple, Any, TypedDict, Callable
 import pandas as pd
+
+import logging
 
 FPAT = re.compile(r"{(?P<fun>\w+)\((?P<args>[a-zA-Z0-9\. ,]+)\)}")
 ALG = "mare"
+
+logger = logging.getLogger(__name__)
+
+
+class Round(TypedDict):
+    pretty: str
+    peek: bool
+    subjects: list[tuple[str, str]]
+
+
+class Experiment(TypedDict):
+    pretty: str
+    type: str
+    dataset_names: dict[str, str]
+
+    datasets: dict[str, Any]
+    rounds: dict[str, Round]
+    generate: Callable[[dict[str, Any], str, Any], str]
 
 
 class MedOnc(NamedTuple):
@@ -15,118 +36,139 @@ class MedOnc(NamedTuple):
     medicine: pd.DataFrame
 
 
-class Datasets(NamedTuple):
-    wrk: MedOnc
-    syn: MedOnc
+class DatasetFunctions(TypedDict):
+    load: Callable[[str], Any]
+    generate: Callable[[dict[str, Any], str, Any], str]
 
 
-def read_data(pq):
-    pd.read_parquet(pq)
-
-
-def load_original_data(ds_path: str):
-    return MedOnc(
-        pd.read_parquet(ds_path + "/view/medonc/tables/patients.pq"),
-        pd.read_parquet(ds_path + "/view/medonc/tables/lines.pq"),
-        pd.read_parquet(ds_path + "/view/medonc/tables/updates.pq"),
-        pd.read_parquet(ds_path + "/view/medonc/tables/medicine.pq"),
-    )
-
-
-def load_syn_data_latest(ds_path: str):
-    ts = sorted(list(os.listdir(ds_path + "/synth/medonc/mare/tables/patients.pq/")))[0]
-    print(f"Synthetic Timestamp is {ts}")
-
-    return load_syn_data_ts(ds_path, ts)
-
-
-def load_syn_data_ts(ds_path: str, ts):
-    return MedOnc(
-        pd.read_parquet(
-            ds_path + "/synth/medonc/mare/tables/patients.pq/" + ts + "/patients.pq"
-        ),
-        pd.read_parquet(
-            ds_path + "/synth/medonc/mare/tables/lines.pq/" + ts + "/lines.pq"
-        ),
-        pd.read_parquet(
-            ds_path + "/synth/medonc/mare/tables/updates.pq/" + ts + "/updates.pq"
-        ),
-        pd.read_parquet(
-            ds_path + "/synth/medonc/mare/tables/medicine.pq/" + ts + "/medicine.pq"
-        ),
-    )
-
-
-def load_data(ds_path: str, ts):
-    return Datasets(
-        load_original_data(ds_path),
-        (
-            load_syn_data_latest(ds_path)
-            if ts is None or ts == "latest"
-            else load_syn_data_ts(ds_path, ts)
-        ),
-    )
-
-
-def generate_patient(ds: Datasets, syn):
+def generate_patient(ds: dict[str, MedOnc], dataset: str, subject: Any):
     out = ""
 
-    patients, lines, updates, medicine = ds.syn if syn else ds.wrk
+    patients, lines, updates, medicine = ds[dataset]
 
-    for id, p in patients.sample(1).iterrows():
-        weight = None
-        bsa = None
-        height = None
+    p = patients[subject]
+    weight = None
+    bsa = None
+    height = None
 
-        out += f'Patient born in {p.birth.year} presents with:\n- {p.primary_icd}: {p.primary_description}, Height: {p.height:.1f}\n'
-        if not math.isnan(p.height):
-            height = p.height
+    out += f"Patient born in {p.birth.year} presents with:\n- {p.primary_icd}: {p.primary_description}, Height: {p.height:.1f}\n"
+    if not math.isnan(p.height):
+        height = p.height
 
-        plines = lines[lines["id"] == id]
-        for i, (lid, l) in enumerate(plines.iterrows()):
-            out += f"> L{i+ 1:02d}/{len(plines)}: {l.protocol}\n"
+    plines = lines[lines["id"] == id]
+    for i, (lid, l) in enumerate(plines.iterrows()):
+        out += f"> L{i+ 1:02d}/{len(plines)}: {l.protocol}\n"
 
-            for j, (uid, u) in enumerate(updates[updates["line_id"] == lid].iterrows()):
-                if not math.isnan(u.bsa):
-                    bsa = u.bsa
+        for j, (uid, u) in enumerate(updates[updates["line_id"] == lid].iterrows()):
+            if not math.isnan(u.bsa):
+                bsa = u.bsa
 
-                if not math.isnan(u.weight):
-                    weight = u.weight
-                    wstr = (
-                        f" {u.weight:5.1f}kg, {u.bsa:.2f} BSA ({u.weight_date.date()})"
-                    )
-                else:
-                    wstr = ""
-                out += f"  {u.date.date()} C{u.cycle:02d}D{u.day:02d}{wstr}\n"
+            if not math.isnan(u.weight):
+                weight = u.weight
+                wstr = f" {u.weight:5.1f}kg, {u.bsa:.2f} BSA ({u.weight_date.date()})"
+            else:
+                wstr = ""
+            out += f"  {u.date.date()} C{u.cycle:02d}D{u.day:02d}{wstr}\n"
 
-                for k, (mid, m) in enumerate(
-                    medicine[medicine["update_id"] == uid].iterrows()
-                ):
-                    # break
-                    notes = m.notes
-                    if isinstance(notes, str):
-                        match = FPAT.match(notes)
-                        if match:
-                            pfuns = {
-                                "fmgm2": lambda d, acc: (
-                                    round(d * bsa / acc) * acc if bsa else "INV: no BSA"
-                                ),
-                                "fmgkg": lambda d, acc: (
-                                    round(d * weight / acc) * acc
-                                    if weight
-                                    else "INV: no weight"
-                                ),
-                                "fauc": lambda d, acc: "AUC:TODO",
-                            }
-                            start, end = match.span()
-                            fn = match.group("fun")
-                            args = eval(match.group("args"))
-                            dosage = pfuns[fn](*args) if fn in pfuns else f"UKN:{fn}"
-                            notes = f"{notes[:start]}{dosage}{notes[end:]}"
+            for k, (mid, m) in enumerate(
+                medicine[medicine["update_id"] == uid].iterrows()
+            ):
+                # break
+                notes = m.notes
+                if isinstance(notes, str):
+                    match = FPAT.match(notes)
+                    if match:
+                        pfuns = {
+                            "fmgm2": lambda d, acc: (
+                                round(d * bsa / acc) * acc if bsa else "INV: no BSA"
+                            ),
+                            "fmgkg": lambda d, acc: (
+                                round(d * weight / acc) * acc
+                                if weight
+                                else "INV: no weight"
+                            ),
+                            "fauc": lambda d, acc: "AUC:TODO",
+                        }
+                        start, end = match.span()
+                        fn = match.group("fun")
+                        args = eval(match.group("args"))
+                        dosage = pfuns[fn](*args) if fn in pfuns else f"UKN:{fn}"
+                        notes = f"{notes[:start]}{dosage}{notes[end:]}"
 
-                    if str(m.drug) != "nan":
-                        out += (
-                            f"   - {str(m.time.time())[:-3]}: {m.drug:>18s}  {notes}\n"
-                        )
+                if str(m.drug) != "nan":
+                    out += f"   - {str(m.time.time())[:-3]}: {m.drug:>18s}  {notes}\n"
 
     return out
+
+
+def load_medonc_data(ds_path: str):
+    return MedOnc(
+        pd.read_parquet(os.path.join(ds_path, "patients.pq")),
+        pd.read_parquet(os.path.join(ds_path, "lines.pq")),
+        pd.read_parquet(os.path.join(ds_path, "updates.pq")),
+        pd.read_parquet(os.path.join(ds_path, "medicine.pq")),
+    )
+
+
+dataset_functions: dict[str, DatasetFunctions] = {
+    "medonc": {"load": load_medonc_data, "generate": generate_patient},
+    "fake": {"load": lambda x: x, "generate": lambda x, y, z: f"{x} {y} {z}"},
+}
+
+
+def load_data(ds_path: str):
+    assert os.path.exists(ds_path), f"Path '{ds_path}' does not exist"
+
+    assert os.path.isdir(ds_path), f"Path '{ds_path}' is not a directory"
+
+    data_info = os.path.join(ds_path, "info.json")
+    assert os.path.exists(data_info), f"File '{data_info}' does not exist"
+
+    with open(data_info, "r") as f:
+        info = json.load(f)
+
+    assert "experiment" in info, f"Key 'experiment' not found in '{data_info}'"
+
+    experiment = info["experiment"]
+
+    experiment_path = os.path.join(ds_path, experiment)
+    assert os.path.isdir(experiment_path), f"Path '{experiment}' not found in {ds_path}"
+
+    info_fn = os.path.join(experiment_path, "info.json")
+    assert os.path.exists(info_fn), f"File '{info_fn}' does not exist"
+
+    with open(info_fn, "r") as f:
+        info = json.load(f)
+
+    pretty = info["pretty"]
+    dataset_type = info["type"]
+    dataset_names = info["datasets"]
+
+    assert dataset_type in dataset_functions, f"Dataset type '{dataset_type}' not found"
+    load_fun = dataset_functions[dataset_type]["load"]
+
+    datasets = {}
+    for k in dataset_names:
+        ds_path = os.path.join(experiment_path, "data", k)
+        assert os.path.exists(ds_path), f"Dataset '{ds_path}' does not exist"
+
+        dataset = load_fun(ds_path)
+        datasets[k] = dataset
+
+    round_dir = os.path.join(experiment_path, "rounds")
+    assert os.path.exists(round_dir), f"Round directory '{round_dir}' does not exist"
+
+    rounds = {}
+    for k in os.listdir(round_dir):
+        rounds[k] = json.load(open(os.path.join(round_dir, k)))
+
+    ds_name_concat = ", ".join([f"'{k}' ({v})" for k, v in dataset_names.items()])
+    logger.info(f"Loaded experiment '{pretty}' with datasets '{ds_name_concat}'")
+    return Experiment(
+        pretty=pretty,
+        type=dataset_type,
+        dataset_names=dataset_names,
+        datasets=datasets,
+        rounds=rounds,
+        generate=dataset_functions[dataset_type]["generate"],
+    )
