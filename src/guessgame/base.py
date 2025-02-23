62817a74583320dd8cb51b12745bab2a579b0777
antheas <antheas@users.noreply.github.com>
2025-02-23 20:18:38 +0000

    remove results from index

--- src/guessgame/base.py
+++ src/guessgame/base.py
@@ -45,7 +45,7 @@ def index():
             if len(v["results"]) < len(v["subjects"]) and not v["finished"]
         },
         history={
-            k: f'{v["name"]} at {experiment["rounds"][v["round"]]["pretty"].split(" (", 1)[0]}, score: {len([v for v in v["results"] if v["result"] == "correct"])}/{len(v["results"])}'
+            k: f'{v["name"]} at {experiment["rounds"][v["round"]]["pretty"].split(" (", 1)[0]}, {len(v["results"])} answers'
             for k, v in sessions.items()
             if (len(v["results"]) == len(v["subjects"]) or v["finished"])
             and len(v["results"]) > 0
